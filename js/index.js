9375e28514d8a49377d1c82e85407e21ebaf4c99
Marcello <marcellorocha632@gmail.com>
2021-11-30 15:48:55 -0300

    Add a js fold to JavaScript files

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let eye = document.querySelector('.eye'),
+inputPassword = document.querySelector('#input-password'),
+enabled = false;
+
+function toggle() {
+    if(enabled) {
+        inputPassword.setAttribute('type', 'password');
+        eye.style.color = '#FE8F8F'
+        enabled = false;
+    }else {
+        inputPassword.setAttribute('type', 'true');
+        eye.style.color = '#FF5C58';
+        enabled = true;
+    }
+}
+
+eye.addEventListener('click', toggle);
