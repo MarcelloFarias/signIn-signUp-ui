c4350089c47c6435871be306fbac5ed1839e2f44
Marcello <marcellorocha632@gmail.com>
2021-12-01 19:35:34 -0300

    Change the identation from variables

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 let eye = document.querySelector('.eye'),
-inputPassword = document.querySelector('#input-password'),
-enabled = false;
+    inputPassword = document.querySelector('#input-password'),
+    enabled = false;
 
 function toggle() {
     if(enabled) {
         inputPassword.setAttribute('type', 'password');
-        eye.style.color = '#FE8F8F'
+        eye.style.color = '#FE8F8F';
         enabled = false;
     }else {
-        inputPassword.setAttribute('type', 'true');
+        inputPassword.setAttribute('type', 'text');
         eye.style.color = '#FF5C58';
         enabled = true;
     }
